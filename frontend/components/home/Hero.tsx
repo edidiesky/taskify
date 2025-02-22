bb1996880fceb93010003dd37a66be2fa7591059
edidiesky <essienedidiong1000@gmail.com>
2025-02-22 03:27:07 -0800

    feat(build fixes): Enhancing the build fixes

--- frontend/components/home/Hero.tsx
+++ frontend/components/home/Hero.tsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Hero = () => {
   const dispatch = useDispatch();
   const { currentUser } = useSelector(
-    (store: { auth: { currentUser?: { name: string } } | null }) => store.auth
+    (store: { auth: { currentUser?: { name: string } | null } }) => store.auth
   );
 
   return (
